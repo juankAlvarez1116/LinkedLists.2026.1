db5cdf851d1c55c923164ee4c5b7a9d70f3363eb
Juan Camilo Alvarez <juancalvarez1116@gmail.com>
2026-05-10 21:03:43 -0500

    Finish exersice

--- LinkedLists.Console/Program.cs
+++ LinkedLists.Console/Program.cs
@@ -1,57 +1,71 @@
 ﻿using DoubleList;
-using SimpleList;
 
-//var list = new SinglyLinkedList<string>();
 var list = new DoubleLinkedList<string>();
+
 var option = string.Empty;
 var value = string.Empty;
+
 do
 {
     option = Menu();
+
     switch (option)
     {
         case "1":
             Console.Write("Enter a value: ");
             value = Console.ReadLine() ?? string.Empty;
-            list.InsertAtBeginning(value);
+
+            list.InsertOrdered(value);
             break;
 
         case "2":
-            Console.Write("Enter a value: ");
-            value = Console.ReadLine() ?? string.Empty;
-            list.InsertAtEnding(value);
-            break;
-
-        case "3":
-            Console.Write("Enter a value: ");
-            value = Console.ReadLine() ?? string.Empty;
-            var exists = list.Contains(value);
-            if (exists)
-            {
-                Console.WriteLine($"Value '{value}' found in the list.");
-            }
-            else
-            {
-                Console.WriteLine($"Value '{value}' not found in the list.");
-            }
-            break;
-
-        case "4":
-            Console.Write("Enter a value: ");
-            value = Console.ReadLine() ?? string.Empty;
-            list.Remove(value);
-            break;
-
-        case "5":
-            list.Reverse();
-            break;
-
-        case "8":
             Console.WriteLine(list.ToString());
             break;
 
-        case "9":
+        case "3":
             Console.WriteLine(list.ToStringReverse());
+            break;
+
+        case "4":
+            list.Sort();
+            Console.WriteLine("List sorted descending.");
+            break;
+
+        case "5":
+            Console.WriteLine(list.GetModes());
+            break;
+
+        case "6":
+            Console.WriteLine(list.GetGraph());
+            break;
+
+        case "7":
+            Console.Write("Enter a value: ");
+            value = Console.ReadLine() ?? string.Empty;
+
+            if (list.Contains(value))
+            {
+                Console.WriteLine("Value exists.");
+            }
+            else
+            {
+                Console.WriteLine("Value does not exist.");
+            }
+
+            break;
+
+        case "8":
+            Console.Write("Enter a value: ");
+            value = Console.ReadLine() ?? string.Empty;
+
+            list.Remove(value);
+            break;
+
+        case "9":
+            Console.Write("Enter a value: ");
+            value = Console.ReadLine() ?? string.Empty;
+
+            list.RemoveAll(value);
             break;
 
         case "0":
@@ -59,23 +73,27 @@ do
             break;
 
         default:
-            Console.WriteLine("Invalid option. Please try again.");
+            Console.WriteLine("Invalid option.");
             break;
     }
+
 } while (option != "0");
 
 string Menu()
 {
-    Console.WriteLine("1. Insert at the beginning");
-    Console.WriteLine("2. Insert at the ending");
-    Console.WriteLine("3. Search for a value");
-    Console.WriteLine("4. Remove a value");
-    Console.WriteLine("5. Reverse list");
-    Console.WriteLine("6. Order list"); // Homework
-    Console.WriteLine("7. Insert ordered"); // Homework
-    Console.WriteLine("8. Show list");
-    Console.WriteLine("9. Show list in reverse");
+    Console.WriteLine("  ");
+    Console.WriteLine("1. Add");
+    Console.WriteLine("2. Show forward");
+    Console.WriteLine("3. Show backward");
+    Console.WriteLine("4. Sort descending");
+    Console.WriteLine("5. Show mode(s)");
+    Console.WriteLine("6. Show graph");
+    Console.WriteLine("7. Exists");
+    Console.WriteLine("8. Remove one occurrence");
+    Console.WriteLine("9. Remove all occurrences");
     Console.WriteLine("0. Exit");
-    Console.Write("Enter your option: ");
+
+    Console.Write("Option: ");
+
     return Console.ReadLine() ?? string.Empty;
 }
